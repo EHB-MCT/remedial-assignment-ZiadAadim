480297a45662706618c26b58204beed4231c4d4e
ZiadAadim <ziad.aadim@student.ehb.be>
2025-08-10 15:50:02 +0200

    minor name change

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
-export const APP_NAME = "tiny-webshop-sim";
+export const APP_NAME = "crypto-shop-sim";
 export const APP_VERSION = "0.0.1";
